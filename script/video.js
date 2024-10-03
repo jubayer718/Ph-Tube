2e2c9903bde14061d85d71dda8bfb2b136566966
jubayer718 <jubayerahmed2462@gmail.com>
2024-10-03 09:01:58 +0700

    video card added

--- script/video.js
+++ script/video.js
@@ -5,6 +5,41 @@ const loadCategories = () => {
     .then(data => displayCategories(data.categories))
     .catch(error => console.log(error));
 };
+
+// get video categories from API
+const loadVideo = () => {
+    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+    .then(res => res.json())
+    .then(data => displayVideos(data.videos))
+    .catch(error => console.log(error));
+}
+// get displayVideos function
+const displayVideos = (videos) => {
+  const videoContainer=document.getElementById('video-container')
+  videos.forEach(videos => {
+    const div = document.createElement('div');
+    div.classList = "card card-compact";
+    div.innerHTML =
+     `
+      <figure>
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+
+    `
+    videoContainer.append(div);
+  })
+}
+
+//show button on display
 const displayCategories = (categories) => {
   const categoryContainer=document.getElementById('categories')
  categories.forEach((item) => {
@@ -22,3 +57,4 @@ const displayCategories = (categories) => {
  });
 }
 loadCategories()
+loadVideo()
