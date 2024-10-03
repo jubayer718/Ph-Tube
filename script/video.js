09b1d75497971714ac61d1496124d6f0cb48d61c
jubayer718 <jubayerahmed2462@gmail.com>
2024-10-03 09:22:14 +0700

    added thumbnail

--- script/video.js
+++ script/video.js
@@ -6,6 +6,28 @@ const loadCategories = () => {
     .catch(error => console.log(error));
 };
 
+
+// const carDemo = {
+// {
+//     "category_id": "1001",
+//     "video_id": "aaaa",
+//     "thumbnail": "https://i.ibb.co/L1b6xSq/shape.jpg",
+//     "title": "Shape of You",
+//     "authors": [
+//         {
+//             "profile_picture": "https://i.ibb.co/D9wWRM6/olivia.jpg",
+//             "profile_name": "Olivia Mitchell",
+//             "verified": ""
+//         }
+//     ],
+//     "others": {
+//         "views": "100K",
+//         "posted_date": "16278"
+//     },
+//     "description": "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
+// }
+// }
+
 // get video categories from API
 const loadVideo = () => {
     fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
@@ -15,16 +37,17 @@ const loadVideo = () => {
 }
 // get displayVideos function
 const displayVideos = (videos) => {
+
   const videoContainer=document.getElementById('video-container')
-  videos.forEach(videos => {
+  videos.forEach(video => {
+    console.log(video)
     const div = document.createElement('div');
     div.classList = "card card-compact";
     div.innerHTML =
      `
-      <figure>
+      <figure class=" h-[200px]">
     <img
-      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-      alt="Shoes" />
+      src=${video.thumbnail} class="w-full h-full object-cover" />
   </figure>
   <div class="card-body">
     <h2 class="card-title">Shoes!</h2>
